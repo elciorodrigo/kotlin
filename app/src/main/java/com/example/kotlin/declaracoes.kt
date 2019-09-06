fa3f59e1bad9ad90fa029aed578b9d79fee68b7e
Elcio Rodrigo <elciorodrigo@gmail.com>
2019-09-06 20:19:36 -0300

    variables and string templates

---
+++ app/src/main/java/com/example/kotlin/declaracoes.kt
@@ -0,0 +1,21 @@
+package com.example.kotlin
+
+fun main(args: Array<String>) {
+
+    // mutaveis - var
+    var frase: String = "Hello, word!"
+
+    println(frase)
+
+    println("$frase")
+
+    println("${frase}")
+
+    println("Qtde de chars: ${frase.count()} ")
+
+    println("Qtde de chars: ${frase.length} ")
+
+    //imutaveis - val
+
+
+}
